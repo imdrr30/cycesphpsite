e3ef9e9b6027020078372993d0716ab5cfc7c092
Revan <43107289+revanrohith@users.noreply.github.com>
2020-09-25 15:27:15 +0000

    Add files via upload

---
+++ defaults/eventheader.php
@@ -0,0 +1,87 @@
+<?php
+require_once "defaults/header.php"; 
+
+$query="SELECT * FROM events ";
+if(isset($_GET['q']) && $_GET['q']!=''){
+  $key1 = mysqli_real_escape_string($con, $_GET['q']);
+  if($where==0){$query.="WHERE ";$where=1;}else{$query.="AND ";}
+  $query.="`title` LIKE '%".$key1."%' ";
+}
+if(isset($_GET['ind']) && $_GET['ind']!=''){
+  $key2 = mysqli_real_escape_string($con, $_GET['ind']);
+  if($where==0){$query.="WHERE ";$where=1;}else{$query.="AND ";}
+  $query.="`indus` LIKE '%".$key2."%' ";
+}
+if(isset($_GET['sec']) && $_GET['sec']!=''){
+  $key3 = mysqli_real_escape_string($con, $_GET['sec']);
+  if($where==0){$query.="WHERE ";$where=1;}else{$query.="AND ";}
+  $query.="`sect` LIKE '%".$key3."%' ";
+}
+if(isset($_GET['date']) && $_GET['date']!=''){
+  $key4 = mysqli_real_escape_string($con, $_GET['date']);
+  if($where==0){$query.="WHERE ";$where=1;}else{$query.="AND ";}
+  $query.="`date` LIKE '%".$key4."%' ";
+}
+if(isset($_GET['loc']) && $_GET['loc']!=''){
+  $key5 = mysqli_real_escape_string($con, $_GET['loc']);
+  if($where==0){$query.="WHERE ";$where=1;}else{$query.="AND ";}
+  $query.="`location` LIKE '%".$key5."%' ";
+}
+if(isset($_GET['pay']) && $_GET['pay']!=''){
+  $key6 = mysqli_real_escape_string($con, $_GET['pay']);
+  if($where==0){$query.="WHERE ";$where=1;}else{$query.="AND ";}
+  $query.="`payment` LIKE '%".$key6."%' ";
+}
+$query.="ORDER BY id DESC;";
+?>
+  <?php if($htitle=="Events") {
+     $result = mysqli_query($con,$query);
+     $rowcount=mysqli_num_rows($result);
+     if($rowcount%5!=0){
+       $rowcount+=5;
+     }
+     $rowcount/=5;
+     $rowcount= (int)$rowcount; 
+    echo '
+  <nav aria-label="Page navigation example" class="float-right" style="margin-top: 10px;">
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>';
+   
+    for($i=1;$i<=$rowcount;$i++){
+      echo '<li class="page-item"><input type="submit" name="page" class="page-link" value="'.$i.'"></li>';
+    }
+    echo
+    '
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+        <span class="sr-only">Next</span>
+      </a>
+    </li>
+  </ul>';} ?>
+</nav>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" style="min-height: 323px;">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark"><?php echo $descrp;?></h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active"><?php echo $htitle;?></li>
+            </ol>
+            
+          </div><!-- /.col -->
+          
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+      </div>
--- events.php
+++ events.php
@@ -1,182 +1,101 @@
 <?php 
-require_once "configurator.php";
-$email = $_SESSION['email'];
-if($email == false){
-  header('Location: login.php');
-}
-$fetch_data = "SELECT * FROM usertable WHERE email = '$email'";
-$run_query = mysqli_query($con, $fetch_data);
-$fetch_info = mysqli_fetch_assoc($run_query);
+$htitle="Events";
+$descrp="All Events";
+require_once "defaults/eventheader.php";
 ?>
-<html style="height: auto;"><head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Events</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-<style type="text/css">/* Chart.js */
-@keyframes chartjs-render-animation{from{opacity:.99}to{opacity:1}}.chartjs-render-monitor{animation:chartjs-render-animation 1ms}.chartjs-size-monitor,.chartjs-size-monitor-expand,.chartjs-size-monitor-shrink{position:absolute;direction:ltr;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1}.chartjs-size-monitor-expand>div{position:absolute;width:1000000px;height:1000000px;left:0;top:0}.chartjs-size-monitor-shrink>div{position:absolute;width:200%;height:200%;left:0;top:0}
+
+<style>
 .col-lg-3 {
     -ms-flex: 0 0 100%;
     flex: 0 0 100%;
     max-width: 100%;
-}</style></head>
-<body class="sidebar-mini layout-fixed" style="height: auto;">
-<div class="wrapper">
-
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a class="nav-link" href="home.php">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a class="nav-link" href="logout.php">Logout</a>
-      </li>
-    </ul>
-
-    <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        
-        
-      </li>
-      <!-- Notifications Dropdown Menu -->
-
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-logout"></i>
-        </a>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index.php" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Cyces</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar os-host os-theme-light os-host-resize-disabled os-host-scrollbar-horizontal-hidden os-host-transition os-host-scrollbar-vertical-hidden"><div class="os-resize-observer-host observed"><div class="os-resize-observer" style="left: 0px; right: auto;"></div></div><div class="os-size-auto-observer observed" style="height: calc(100% + 1px); float: left;"><div class="os-resize-observer"></div></div><div class="os-content-glue" style="margin: 0px -8px; width: 249px; height: 322px;"></div><div class="os-padding"><div class="os-viewport os-viewport-native-scrollbars-invisible" style=""><div class="os-content" style="padding: 0px 8px; height: 100%; width: 100%;">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block"><?php echo $fetch_info['email'] ?></a>
-        </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
-            <a class="nav-link" href="dashboard.php">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                
-              </p>
-            </a>
-            
-          </li>
-          <li class="nav-item has-treeview menu-open">
-            
-            <a class="nav-link active" href="events.php">
-              <i class="nav-icon fas fa-chart-pie"></i>
-              <p>Events
-              </p>
-            </a>
-          </li>
-          <?php
-          if ($_SESSION['role']=='ADMIN'){
-            echo '
-          <li class="nav-item has-treeview menu-open">
-            <a class="nav-link" href="addevent.php">
-              <i class="nav-icon fas fa-edit"></i>
-              <p>Add Event
-              </p>
-            </a>';}
-            ?>
-            
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div></div></div><div class="os-scrollbar os-scrollbar-horizontal os-scrollbar-unusable os-scrollbar-auto-hidden"><div class="os-scrollbar-track"><div class="os-scrollbar-handle" style="width: 100%; transform: translate(0px);"></div></div></div><div class="os-scrollbar os-scrollbar-vertical os-scrollbar-unusable os-scrollbar-auto-hidden"><div class="os-scrollbar-track"><div class="os-scrollbar-handle" style="height: 100%; transform: translate(0px);"></div></div></div><div class="os-scrollbar-corner"></div></div>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper" style="min-height: 323px;">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">All Events</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Events</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+}
+</style>
 
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
-        
+        <div class="row">
+<div class="btn-group">
+  <select name="ind" class="btn btn-primary dropdown-toggle" style="margin-left: 20px;margin-bottom: 15px;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">  
+  <div class="dropdown-menu">
+    <option class="dropdown-item" value="<?php
+    if(isset($_GET['ind']) && $_GET['ind']!=''){
+      echo $_GET['ind'].'" selected="selected';
+    }
+    ?>">Industry</option>
+    
+    <option class="dropdown-item" value="Engineering">Engineering</option>
+    <option class="dropdown-item" value="Music">Music</option>
+    <option class="dropdown-item" value="Education">Education</option>
+  </div>
+  </select>
+
+
+
+  <select name="sec" class="btn btn-primary dropdown-toggle" style="margin-left: 10px;margin-bottom: 15px;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">  
+  <div class="dropdown-menu">
+    <option class="dropdown-item" value="<?php
+    if(isset($_GET['sec']) && $_GET['sec']!=''){
+      echo $_GET['sec'].'" selected="selected';
+    }
+    ?>">Sector</option>
+    <option class="dropdown-item" value="English">English</option>
+    <option class="dropdown-item" value="Tamil">Tamil</option>
+    <option class="dropdown-item" value="Computer Science">Computer</option>
+  </div>
+  </select>
+
+  <select name="loc" class="btn btn-primary dropdown-toggle" style="margin-left: 10px;margin-bottom: 15px;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">  
+  <div class="dropdown-menu">
+    <option class="dropdown-item" value="<?php
+    if(isset($_GET['loc']) && $_GET['loc']!=''){
+      echo $_GET['loc'].'" selected="selected';
+    }
+    ?>">Location</option>
+    <option class="dropdown-item" value="Chennai">Chennai</option>
+    <option class="dropdown-item" value="Madurai">Madurai</option>
+    <option class="dropdown-item" value="Coimbatore">Coimbatore</option>
+    <option class="dropdown-item" value="Mumbai">Mumbai</option>
+  </div>
+  </select>
+
+  <select name="date" class="btn btn-primary dropdown-toggle" style="margin-left: 10px;margin-bottom: 15px;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">  
+  <div class="dropdown-menu">
+    <option class="dropdown-item" value="<?php
+    if(isset($_GET['date']) && $_GET['date']!=''){
+      echo $_GET['date'].'" selected="selected';
+    }
+    ?>">Date</option>
+    <option class="dropdown-item" value="today">Today</option>
+    <option class="dropdown-item" value="tomorrow">Tomorrow</option>
+    <option class="dropdown-item" value="week">Within a Week</option>
+    <option class="dropdown-item" value="month">Within a Month</option>
+  </div>
+  </select>
+  <select name="pay" class="btn btn-primary dropdown-toggle" style="margin-left: 10px;margin-bottom: 15px;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">  
+  <div class="dropdown-menu">
+    <option class="dropdown-item" value="<?php
+    if(isset($_GET['pay']) && $_GET['pay']!=''){
+      echo $_GET['pay'].'" selected="selected';
+    }
+    ?>">Free or Paid</option>
+    <option class="dropdown-item" value="free">Free</option>
+    <option class="dropdown-item" value="paid">Paid</option>
+  </div>
+  </select>
+  <button class="btn btn-secondary" style="margin-left: 10px;margin-bottom: 15px;">Apply Filter</button>
+</div>
+</form>
         <?php
-        $result = mysqli_query($con,"SELECT * FROM events");
+        $count=1;
+        $max=5;
+        if(isset($_GET['page'])){
+          $max*=$_GET['page'];
+        }
         while($row = mysqli_fetch_array($result)){
+          if($count>=$max-4 && $count<=$max){
             echo '
         <div class="col-lg-3 col-6">
             <!-- small box -->
@@ -193,9 +112,11 @@ $fetch_info = mysqli_fetch_assoc($run_query);
               <a href="'.$row['reglink'].'" class="small-box-footer">'.$row['payment'].' <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>';
-          
+          }
+          $count++;
         }
         ?>
+        
         <!-- /.row -->
         <!-- Main row -->
         
@@ -203,26 +124,4 @@ $fetch_info = mysqli_fetch_assoc($run_query);
     </section>
     <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
-  
-
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-
-<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<?php require "defaults/footer.php";?>
